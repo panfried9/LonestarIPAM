87171f94b7e015582e8007fce8ade740eee7a085
panfried9 <panfried9@gmail.com>
2023-07-05 13:31:13 -0700

    added type for status

--- init-postgres.py
+++ init-postgres.py
@@ -7,7 +7,9 @@ sql_statement = []
 sql_statement.append("DROP TABLE ipam;")
 sql_statement.append("DROP TABLE users;")
 sql_statement.append("DROP TABLE workspaces;")
-sql_statement.append("CREATE TABLE ipam (FIRST INET PRIMARY KEY, LAST INET, VRF CHAR(50), WORKSPACE CHAR(50), COMMENT CHAR(200) );")
+sql_statement.append("DROP TYPE status CASCADE;")
+sql_statement.append("CREATE TYPE status AS ENUM ('unassigned','available', 'in-use-set', 'in-use-found', 'reserved', 'capped' );") 
+sql_statement.append("CREATE TABLE ipam (first INET PRIMARY KEY, last INET, vrf CHAR(50), workspace CHAR(50), comment CHAR(200) , current_status status );")
 sql_statement.append("CREATE TABLE users (USERNAME CHAR(50), PASSWORD CHAR(50));")
 sql_statement.append("CREATE TABLE workspaces (NAME CHAR(50), USERNAME CHAR(50));")
 
